2ad42861f23b7a9e5b330f97db24becfe5efe40d
chayakorn11062549@gmail.com <chayakorn11062549@gmail.com>
2026-01-21 15:30:16 +0700

    third

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,4 +1,9 @@
 const x = Number(process.argv[2]);
 
-if ((isNaN(x)) || x <= 0 ){
-}  console.log("");
+if (isNaN(x) || x < 0){
+    console.log("");
+} else if (x % 3){
+    console.log("Fizz");
+} else if (x % 5){
+    console.log("Buzz");
+} else console.log(x)
